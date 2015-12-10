4cfd89d10836502302fb8984dcb6ed5a793d5d6c
eyelidlessness <gnosis@gmail.com>
2015-12-10 14:03:05 -0500

    Deploy a change

--- src/try_shippable/wat.cljs
+++ src/try_shippable/wat.cljs
@@ -2,3 +2,10 @@
 
 (defn wat? [wat]
   (js/console.log "Wat?" wat))
+
+(defn a-change! []
+  (let [p (js/document.createElement "p")]
+    (set! (.-innerHTML p) "Appended")
+    (js/document.body.appendChild p)))
+
+(a-change!)
